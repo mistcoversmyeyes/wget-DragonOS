58e3b89f4742a2f7b03698952098a1f30f18928a
Yuming <mingjiangyu1@qq.com>
2025-07-25 22:04:03 +0800

    refactor: 添加新的HttpRequest结构体设计
    - 将HttpRequest从enum重构为struct HttpRequestNew
    - 添加HttpRequestHeader和HttpHeaderField结构体
    - 保留旧enum代码以确保向后兼容
    - 添加基础构造方法(new, get, post, head)
    - 实现Display trait用于HTTP报文格式化

--- wget/src/web/http/http_requests.rs
+++ wget/src/web/http/http_requests.rs
@@ -1,24 +1,50 @@
-use std::{error::Error, fmt::Display};
+use std::fmt::Display;
 
-
-pub enum HttpProtocolVersion {
-    Http09,
-    Http10,
-    Http11,
-    Http20,
-    Http30, 
+pub struct HttpRequestNew<'a> {
+    method: HttpMethod,
+    path_to_file: &'a str,
+    protocol_version: HttpProtocolVersion,
+    request_header: HttpRequestHeader,
 }
 
-impl Display for HttpProtocolVersion {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        let s = match self {
-            HttpProtocolVersion::Http09 => "HTTP/0.9",
-            HttpProtocolVersion::Http10 => "HTTP/1.0",
-            HttpProtocolVersion::Http11 => "HTTP/1.1",
-            HttpProtocolVersion::Http20 => "HTTP/2.0",
-            HttpProtocolVersion::Http30 => "HTTP/3.0",
-        };
-        write!(f, "{}", s)
+impl<'a> HttpRequestNew<'a> {
+    /// 创建一个新的 HTTP 请求
+    pub fn new(
+        method: HttpMethod,
+        path: &'a str,
+        protocol_version: HttpProtocolVersion,
+        headers: Vec<HttpHeaderField>,
+    ) -> Self {
+        Self {
+            method,
+            path_to_file: path,
+            protocol_version,
+            request_header: HttpRequestHeader { fields: headers },
+        }
+    }
+
+    /// 创建一个 GET 请求
+    pub fn get(path: &'a str, headers: Vec<HttpHeaderField>) -> Self {
+        Self::new(HttpMethod::GET, path, HttpProtocolVersion::Http11, headers)
+    }
+
+    /// 创建一个 POST 请求
+    pub fn post(path: &'a str, headers: Vec<HttpHeaderField>) -> Self {
+        Self::new(HttpMethod::POST, path, HttpProtocolVersion::Http11, headers)
+    }
+
+    /// 创建一个 HEAD 请求
+    pub fn head(path: &'a str, headers: Vec<HttpHeaderField>) -> Self {
+        Self::new(HttpMethod::HEAD, path, HttpProtocolVersion::Http11, headers)
+    }
+
+    /// 添加请求头
+    // TODO: 重命名函数名称为 append_header_field()
+    pub fn add_header(&mut self, name: String, value: String) {
+        self.request_header.fields.push(HttpHeaderField {
+            field_name: name,
+            field_value: value,
+        });
     }
 }
 
@@ -55,6 +81,115 @@ impl Display for HttpRequest<'_> {
         }
     }
 }
+
+
+impl Display for HttpRequestNew<'_> {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{} {} {}\r\n{}\r\n", self.method, self.path_to_file, self.protocol_version, self.request_header)
+    }
+}
+
+pub enum HttpMethod {
+    /// GET 方法用于请求指定资源（最低支持 HTTP/0.9）
+    GET,
+    /// POST 方法向指定资源提交要处理的数据（最低支持 HTTP/1.0）
+    POST,
+    /// HEAD 方法请求与 GET 请求相同的响应，但没有响应体（最低支持 HTTP/1.0）
+    HEAD,
+    /// PUT 方法用请求负载替换目标资源的所有当前表示（最低支持 HTTP/1.1）
+    PUT,
+    /// DELETE 方法删除指定资源（最低支持 HTTP/1.1）
+    DELETE,
+    /// CONNECT 方法建立到目标资源服务器的隧道（最低支持 HTTP/1.1）
+    CONNECT,
+    /// OPTIONS 方法描述目标资源的通信选项（最低支持 HTTP/1.1）
+    OPTIONS,
+    /// TRACE 方法沿着到目标资源的路径执行消息回环测试（最低支持 HTTP/1.1）
+    TRACE,
+    /// PATCH 方法对资源进行部分修改（最低支持 HTTP/1.1）
+    PATCH,
+}
+
+pub enum HttpProtocolVersion {
+    Http09,
+    Http10,
+    Http11,
+    Http20,
+    Http30, 
+}
+
+
+impl Display for HttpMethod {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            HttpMethod::GET => "GET",
+            HttpMethod::POST => "POST",
+            HttpMethod::HEAD => "HEAD",
+            HttpMethod::PUT => "PUT",
+            HttpMethod::DELETE => "DELETE",
+            HttpMethod::CONNECT => "CONNECT",
+            HttpMethod::OPTIONS => "OPTIONS",
+            HttpMethod::TRACE => "TRACE",
+            HttpMethod::PATCH => "PATCH",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+impl Display for HttpProtocolVersion {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        let s = match self {
+            HttpProtocolVersion::Http09 => "HTTP/0.9",
+            HttpProtocolVersion::Http10 => "HTTP/1.0",
+            HttpProtocolVersion::Http11 => "HTTP/1.1",
+            HttpProtocolVersion::Http20 => "HTTP/2.0",
+            HttpProtocolVersion::Http30 => "HTTP/3.0",
+        };
+        write!(f, "{}", s)
+    }
+}
+
+pub struct HttpRequestHeader {
+    fields: Vec<HttpHeaderField>,
+}
+
+#[derive(Clone)]
+pub struct HttpHeaderField {
+    pub field_name: String,
+    pub field_value: String,
+}
+
+impl HttpHeaderField {
+    pub fn new(name: String, value: String) -> Self {
+        Self {
+            field_name: name,
+            field_value: value,
+        }
+    }
+}
+
+impl Display for HttpRequestHeader {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        // Rust 闭包示例 - 自动推导捕获方式
+        self.fields.iter().try_for_each(|field| {
+            write!(f, "{}: {}\r\n", field.field_name, field.field_value)
+        })
+        
+        // C++ lambda 等价代码（伪代码注释）：
+        // std::for_each(fields.begin(), fields.end(), 
+        //     [&f](const auto& field) {  // [&f] 显式指定按引用捕获 f
+        //         f << field.field_name << ": " << field.field_value << "\r\n";
+        //     });
+    }
+}
+
+impl Display for HttpHeaderField {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f,"{}: {}",self.field_name, self.field_value)
+    }    
+}
+
+
                         
 #[cfg(test)]
 mod tests {
